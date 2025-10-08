9667abc2abf5e1d594e01eb18fc68bdc016a4e20
NihalTalla <nihaltalla3430@gmail.com>
2025-10-08 20:18:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const path = require('path');
 const otpGenerator = require('otp-generator');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // ---- Security / proxy ----
 app.set('trust proxy', 1);
