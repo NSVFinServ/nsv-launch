33cdc301bda0478f758d1d929ba59191577899ff
NihalTalla <nihaltalla3430@gmail.com>
2025-11-08 22:57:29 +0530

    Add AOS attributes to services section
    Added AOS animation attributes to the services section.

--- src/Services.tsx
+++ src/Services.tsx
@@ -64,7 +64,7 @@ const Services = () => {
   ];
 
   return (
-    <section id="services" className="py-16 bg-white">
+    <section id="services" className="py-16 bg-white" data-aos="fade-left" data-aos-delay="100">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Header */}
         <div className="text-center mb-12">
