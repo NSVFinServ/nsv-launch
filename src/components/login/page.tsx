b9b263a69d93e6419e7f02f39b8ec9992f69a327
jayarudrachalikwar <jayarudrachalikwar@gmail.com>
2025-11-09 12:01:29 +0530

    Import sendLoginNotification in LoginPage component

--- src/components/login/page.tsx
+++ src/components/login/page.tsx
@@ -6,6 +6,7 @@ import { Input } from "../../components/ui/input"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../../components/ui/card"
 import logo from '../../components/logo.png'
 import { API_BASE_URL, withApi } from '@/lib/api.ts';
+import { sendLoginNotification } from '@/lib/loginNotification';
 
 export default function LoginPage() {
   const [showPassword, setShowPassword] = useState(false)
