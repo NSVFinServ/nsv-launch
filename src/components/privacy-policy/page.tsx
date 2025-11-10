b1d61022682126e5ae1f60e5b04a02fd5df8ffc7
NihalTalla <nihaltalla3430@gmail.com>
2025-11-10 20:57:37 +0530

    Update page.tsx

--- src/components/privacy-policy/page.tsx
+++ src/components/privacy-policy/page.tsx
@@ -116,7 +116,8 @@ const PrivacyPolicy = () => {
               <p><strong>NSV Finserv</strong></p>
               <p>Email: nsvfinserv@gmail.com</p>
               <p>Phone: +91-9885885847</p>
-              <p>Address: [Your Business Address]</p>
+              <p>Address: 27-16-31/2a , Sri Krishna Nagar Colony, Neredmet,<br />
+                  Secunderabad - 500056, Telangana</p>
             </div>
 
           </div>
