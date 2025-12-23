656d4ca76ae34e05f85dfd5c043fa25ffb927ffd
jayarudrachalikwar <jayarudrachalikwar@gmail.com>
2025-12-23 14:49:24 +0530

    Add deduplication for react and react-dom

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { resolve } from 'path';
 export default defineConfig({
   plugins: [react()],
   resolve: {
+    dedupe: ['react', 'react-dom'],
     alias: {
       '@': resolve('./src'),
     },
