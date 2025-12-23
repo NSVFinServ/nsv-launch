c6846a7eca213d230afbd43993848c752cc4e165
jayarudrachalikwar <jayarudrachalikwar@gmail.com>
2025-12-23 23:23:13 +0530

    Refactor Blogs component to include state management

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -1,5 +1,23 @@
-useEffect(() => {
-  fetch(`${API_BASE_URL}/blogs`)
-    .then(res => res.json())
-    .then(setBlogs);
-}, []);
+import { useEffect, useState } from 'react';
+import { API_BASE_URL } from '@/lib/api';
+
+const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    fetch(`${API_BASE_URL}/blogs`)
+      .then(res => res.json())
+      .then(setBlogs);
+  }, []);
+
+  return (
+    <div>
+      <h1>Blogs</h1>
+      {blogs.map((b: any) => (
+        <div key={b.id}>{b.title}</div>
+      ))}
+    </div>
+  );
+};
+
+export default Blogs;
