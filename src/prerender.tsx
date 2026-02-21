fb9abbd5ac8eb5508a10d13ce9e3365bb0af6d4a
Anant Mishra <anantmishra12b1@gmail.com>
2026-02-21 10:46:16 +0530

    Update prerender.tsx

--- src/prerender.tsx
+++ src/prerender.tsx
@@ -1,14 +1,28 @@
 import React from "react";
 import { renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router";
-import AppRoutes from "./Router"; // (we’ll create this shared route tree)
+import { HelmetProvider } from "react-helmet-async";
+import AppRoutes from "./Router";
 
 export async function prerender(data: { url: string }) {
+  const helmetContext: any = {};
+
   const html = renderToString(
-    <StaticRouter location={data.url}>
-      <AppRoutes />
-    </StaticRouter>
+    <HelmetProvider context={helmetContext}>
+      <StaticRouter location={data.url}>
+        <AppRoutes />
+      </StaticRouter>
+    </HelmetProvider>
   );
 
-  return { html };
+  // vite-prerender-plugin can consume `head` if provided
+  const { helmet } = helmetContext;
+
+  const head =
+    (helmet?.title?.toString?.() || "") +
+    (helmet?.meta?.toString?.() || "") +
+    (helmet?.link?.toString?.() || "") +
+    (helmet?.script?.toString?.() || "");
+
+  return { html, head };
 }
