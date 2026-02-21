9fe8ed1e105ad7b75b82bbf3a62385744d4a5062
Anant Mishra <anantmishra12b1@gmail.com>
2026-02-21 12:22:08 +0530

    Pass prerenderData to AppRoutes component

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,17 @@ import { HelmetProvider } from "react-helmet-async";
 import AppRoutes from "./Router";
 import "./index.css";
 
+declare global {
+  interface Window {
+    __PRERENDER_DATA__?: any;
+  }
+}
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <HelmetProvider>
       <BrowserRouter>
-        <AppRoutes />
+        <AppRoutes prerenderData={window.__PRERENDER_DATA__} />
       </BrowserRouter>
     </HelmetProvider>
   </React.StrictMode>
