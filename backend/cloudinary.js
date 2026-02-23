33d2e8b9e9a9f397ca38e55d31f2d033a839be30
Anant Mishra <anantmishra12b1@gmail.com>
2026-02-23 14:42:49 +0530

    Update cloudinary.js

--- backend/cloudinary.js
+++ backend/cloudinary.js
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: "dkg3sps2u",
-  api_key: "736356749136349",
-  api_secret: "0OTthP8KJlA6BUYxh82AwUgin58",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
