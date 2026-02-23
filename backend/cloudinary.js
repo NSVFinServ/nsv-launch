00ae4d59a1ee759fc671663fb7ee206e4ff5ce45
Anant Mishra <anantmishra12b1@gmail.com>
2026-02-23 11:28:35 +0530

    Create cloudinary.js

---
+++ backend/cloudinary.js
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: "dkg3sps2u",
+  api_key: "736356749136349",
+  api_secret: "0OTthP8KJlA6BUYxh82AwUgin58",
+});
+
+export default cloudinary;
